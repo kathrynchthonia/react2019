ba2a2e920556f09077ce9ad84abcde1847d4eef8
twheelertech <tony@wheelertechconsulting.com>
2019-09-20 00:57:34 -0500

    added users

---
+++ src/components/users/Users.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import UserItem from "./UserItem";
+
+class Users extends Component {
+  state = {
+    users: [
+      {
+        id: "id",
+        login: "mojombo",
+        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
+        html_url: "https://github.com/mojombo"
+      },
+      {
+        id: "id",
+        login: "defunkt",
+        avatar_url: "https://avatars0.githubusercontent.com/u/2?v=4",
+        html_url: "https://github.com/defunkt"
+      },
+      {
+        id: "id",
+        login: "wycats",
+        avatar_url: "https://avatars0.githubusercontent.com/u/4?v=4",
+        html_url: "https://github.com/wycats"
+      }
+    ]
+  };
+  render() {
+    return (
+      <div style={userStyle}>
+        {this.state.users.map(user => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    );
+  }
+}
+
+const userStyle = {
+  display: "grid",
+  gridTemplateColumns: "repeat(3, 1fr)",
+  gridGap: "1rem"
+};
+
+export default Users;
